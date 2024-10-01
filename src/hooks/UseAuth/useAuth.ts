0ab5c71ca416ddfdbf9c5c119914f23fe30697b7
Mariano Luque <mn.luque.14@gmail.com>
2024-10-01 18:40:43 -0300

    Fix de manejo de perfiles

--- src/hooks/UseAuth/useAuth.ts
+++ src/hooks/UseAuth/useAuth.ts
@@ -3,107 +3,129 @@ import { getDatosUsuario } from "../../App/Auth/Cuenta";
 import { PERFILES } from "../../App/consts/UsuarioConsts";
 
 type TData = {
-  perfiles: any[];
-  usuario: any;
-  tienePerfil: boolean;
-  isLogged: boolean;
-  perfilSeleccionado: number;
+	perfiles: any[];
+	usuario: any;
+	tienePerfil: boolean;
+	isLogged: boolean;
+	perfilSeleccionado: number;
 };
 
 interface IUseProvideAuth {}
 export const useProvideAuth = (props: any) => {
-  const [data, setData] = useState<TData>({
-    perfiles: [],
-    usuario: null,
-    tienePerfil: false,
-    isLogged: false,
-    perfilSeleccionado: -1,
-  });
+	const [data, setData] = useState<TData>({
+		perfiles: [],
+		usuario: null,
+		tienePerfil: false,
+		isLogged: false,
+		perfilSeleccionado: -1,
+	});
 
-  const handleGetDatosUsuario = () => {
-    getDatosUsuario()
-      .then((response: any) => {
-        const perfiles = response.data.perfilesUsuario.map(
-          (perfil: any) => perfil
-        );
-        let usuario = null;
-        if (response.data.tiene_perfil)
-          usuario = {
-            nombre: response.data.datos_usuario_perfil.datos_turista.nombre,
-            apellido: response.data.datos_usuario_perfil.datos_turista.apellido,
-            mail: response.data.datos_usuario_perfil.datos_turista.mail,
-          };
-        const tiene_perfil = response.data.tiene_perfil;
-        const isLogged = true;
+	const handleGetDatosUsuario = () => {
+		getDatosUsuario()
+			.then((response: any) => {
+				const perfiles = response.data.perfilesUsuario.map(
+					(perfil: any) => perfil
+				);
+				let usuario = null;
+				if (response.data.tiene_perfil) {
+					if (
+						response.data.perfilesUsuario[0].id_perfil ==
+						PERFILES.TURISTA.id
+					) {
+						usuario = {
+							nombre: response.data.datos_usuario_perfil
+								.datos_turista.nombre,
+							apellido:
+								response.data.datos_usuario_perfil.datos_turista
+									.apellido,
+							mail: response.data.datos_usuario_perfil
+								.datos_turista.mail,
+						};
+					} else {
+						usuario = {
+							nombre: response.data.datos_usuario_perfil
+								.datos_prestador.nombre,
+							apellido:
+								response.data.datos_usuario_perfil
+									.datos_prestador.apellido,
+							mail: response.data.datos_usuario_perfil
+								.datos_prestador.mail,
+						};
+					}
+				}
+				const tiene_perfil = response.data.tiene_perfil;
+				const isLogged = true;
+				const perfilSeleccionado =
+					perfiles.length > 0 ? perfiles[0].id_perfil : -1;
 
-        setData((prev: TData) => ({
-          perfiles: perfiles,
-          usuario: usuario,
-          tienePerfil: tiene_perfil,
-          isLogged: isLogged,
-          perfilSeleccionado: perfiles.length > 0 ? 0 : -1,
-        }));
+				setData((prev: TData) => ({
+					perfiles: perfiles,
+					usuario: usuario,
+					tienePerfil: tiene_perfil,
+					isLogged: isLogged,
+					perfilSeleccionado: perfilSeleccionado,
+				}));
 
-        if (
-          !response.data.tiene_perfil &&
-          !window.location.toString().includes("signup/complete")
-        )
-          window.location.replace("signup/complete");
-      })
-      .catch((error: any) => {
-        setData((prev: TData) => ({
-          perfiles: [],
-          usuario: null,
-          tienePerfil: false,
-          isLogged: false,
-          perfilSeleccionado: -1,
-        }));
-      });
-  };
+				if (
+					!response.data.tiene_perfil &&
+					!window.location.toString().includes("signup/complete")
+				)
+					window.location.replace("signup/complete");
+			})
+			.catch((error: any) => {
+				setData((prev: TData) => ({
+					perfiles: [],
+					usuario: null,
+					tienePerfil: false,
+					isLogged: false,
+					perfilSeleccionado: -1,
+				}));
+			});
+	};
 
-  useEffect(() => {
-    handleGetDatosUsuario();
-  }, []);
+	useEffect(() => {
+		handleGetDatosUsuario();
+	}, []);
 
-  const logout = () => {
-    setData((prev: TData) => ({
-      perfiles: [],
-      usuario: null,
-      tienePerfil: false,
-      isLogged: false,
-      perfilSeleccionado: -1,
-    }));
-    // window.location.reload();
-  };
+	const logout = () => {
+		setData((prev: TData) => ({
+			perfiles: [],
+			usuario: null,
+			tienePerfil: false,
+			isLogged: false,
+			perfilSeleccionado: -1,
+		}));
+		// window.location.reload();
+	};
 
-  const login = () => {
-    handleGetDatosUsuario();
-  };
+	const login = () => {
+		handleGetDatosUsuario();
+	};
 
-  const cambiarPerfil = () => {
-    if (data.perfilSeleccionado == -1) return;
-    if (!data) return;
-    setData((prev: TData) => {
-      let next;
-      if (prev.perfilSeleccionado == -1) next = -1;
-      if (prev.perfilSeleccionado == prev.perfiles.length - 1) next = 0;
-      else next = prev.perfilSeleccionado + 1;
-      return { ...prev, perfilSeleccionado: next };
-    });
-    console.log("Llega")
-  };
+	const cambiarPerfil = () => {
+		if (data.perfilSeleccionado == -1) return;
+		if (!data) return;
+		setData((prev: TData) => {
+			let next;
+			if (prev.perfilSeleccionado == -1) next = -1;
+			if (prev.perfilSeleccionado == prev.perfiles.length - 1) next = 0;
+			else next = prev.perfilSeleccionado + 1;
+			return { ...prev, perfilSeleccionado: next };
+		});
+		console.log("Llega");
+	};
 
-  const getPerfil = (): number => {
-    if (!data || data.perfiles.length == 0 || data.perfilSeleccionado == -1)
-      return PERFILES.INVITADO.id;
-    return data.perfiles[data.perfilSeleccionado].id_perfil;
-  };
+	const getPerfil = (): number => {
+		if (!data || data.perfiles.length == 0 || data.perfilSeleccionado == -1)
+			return PERFILES.INVITADO.id;
+		return data.perfiles[0].id_perfil;
+	};
 
-  return {
-    ...data,
-    logout,
-    login,
-    cambiarPerfil,
-    getPerfil,
-  };
+	return {
+		...data,
+		logout,
+		login,
+		cambiarPerfil,
+		getPerfil,
+	};
 };

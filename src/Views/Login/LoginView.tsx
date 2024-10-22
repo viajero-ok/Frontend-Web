4e917784b0118590a3516dadddd6f5e604930035
Mariano Luque <mn.luque.14@gmail.com>
2024-10-21 21:43:01 -0300

    fix ionpage

--- src/Views/Login/LoginView.tsx
+++ src/Views/Login/LoginView.tsx
@@ -22,7 +22,7 @@ export default function LoginView(props: any) {
   };
 
   return (
-    <IonContent>
+    <>
       <IonCard
         style={{
           backgroundColor: "rgba(255, 255, 255, 1)",
@@ -79,6 +79,6 @@ export default function LoginView(props: any) {
           </IonCol>
         </IonRow>
       </IonGrid> */}
-    </IonContent>
+    </>
   );
 }
